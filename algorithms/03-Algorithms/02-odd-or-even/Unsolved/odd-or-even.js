b64dd9f6d27ca3b2a93c66fb2c4011a01d1de50a
emilypier <piersoee@miamioh.edu>
2022-04-06 16:08:57 -0400

    completed 03 algorithms

--- algorithms/03-Algorithms/02-odd-or-even/Unsolved/odd-or-even.js
+++ algorithms/03-Algorithms/02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,12 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function(num) {};
+var oddOrEven = function(num) {
+  // if num evenly divisible by 2, return even
+  if (num % 2 === 0) {
+    return "even";
+  // if not, return odd
+  } else {
+    return "odd"
+  }
+};
--- algorithms/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ algorithms/03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,10 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+  //loop through numbers 1 to num
+  // starting at 1, until num is less than or equal to 1, increment through numbers.
+  for (var i = 1; i <= num; i++) {
+    console.log(i);
+  };
+};
