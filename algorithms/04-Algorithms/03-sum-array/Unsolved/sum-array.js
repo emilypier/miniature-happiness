537becd98b959561dad0194cb1b5a0e4115888f7
emilypier <piersoee@miamioh.edu>
2022-04-16 16:15:13 -0400

    sumArray algorithm

--- algorithms/04-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ algorithms/04-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,12 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+const sumArray = function(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentNumber = arr[i];
+    result += currentNumber;
+  }
+
+  return result;
+};
