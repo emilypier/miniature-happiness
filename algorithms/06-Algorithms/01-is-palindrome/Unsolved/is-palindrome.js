d11dbdfd67cc692f2a5079750a124c236d9e6258
emilypier <piersoee@miamioh.edu>
2022-04-28 15:59:39 -0400

    word palindrome algorithm

--- algorithms/06-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ algorithms/06-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,15 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+  // get reveresed string
+  var reversedString = str
+    .split('') // splits each letter
+    .reverse('') // reverses order of letters
+    .join(''); // joins all letters with nothing between them
+
+  if (reversedString === str) {
+    return true;
+  } else {
+    return false;
+  }  
+};
