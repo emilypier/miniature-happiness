57857cff0435452100c2e4a5313a03a185884612
emilypier <piersoee@miamioh.edu>
2022-04-28 16:36:42 -0400

    fizzBuzz algorithm

--- algorithms/05-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ algorithms/05-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,38 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(arr) {
+  // loop through array of numbers
+  for (var i = 0; i < arr.length; i++) {
+    var currentNumber = arr[i];
+
+    if (currentNumber % 15 === 0) {
+      console.log("Fizz Buzz");
+    } else if (currentNumber % 3 === 0) {
+      console.log("Fizz");
+    } else if (currentNumber % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(currentNumber);
+    }
+  }
+};
+
+
+// THIS DOESN'T WORK
+// var fizzBuzz = function(arr) {
+//   // loop through array of numbers
+//   for (var i = 0; i < arr.length; i++) {
+//     var currentNumber = arr[i];
+
+//     if (currentNumber % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (currentNumber % 5 === 0) {
+//       console.log("Buzz");
+//     } else if (currentNumber % 15 === 0) {
+//         console.log("Fizz Buzz");
+//     } else {
+//       console.log(currentNumber);
+//     }
+//   }
+// };
