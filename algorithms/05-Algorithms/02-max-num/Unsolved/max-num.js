a60d9f2f031ccc421d3d79ab5a27f42635e23b29
emilypier <piersoee@miamioh.edu>
2022-04-28 16:50:13 -0400

    max number algorithm

--- algorithms/05-Algorithms/02-max-num/Unsolved/max-num.js
+++ algorithms/05-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,27 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+// sort numbers in array in descending order
+  arr.sort(
+    function(a, b){
+      return b - a;
+    }
+  );
+    return (arr[0]);
+};
+
+// // OR
+// var maxNum = function (arr) {
+//   console.log(arr);
+//   var max = arr[0];
+
+//   for (var i = 1; i < arr.length; i++) {
+//     var currentNum = arr[i];
+
+//     if (currentNum > max) {
+//       max = currentNum;
+//     }
+//   }
+
+//   return max;
+// };
