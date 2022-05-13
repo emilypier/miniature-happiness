1727fee3bc245b2f8175fa983956b61d00e18f43
emilypier <piersoee@miamioh.edu>
2022-05-13 15:27:08 -0400

    boolean to string algorithm

---
+++ algorithms/codewars-algorithms/remove-string-spaces/boolean-to-string.js
@@ -0,0 +1,8 @@
+// Implement a function which convert the given boolean value into its string representation.
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+function booleanToString(b){
+  let newString = b.toString();
+      
+  return newString;
+}
