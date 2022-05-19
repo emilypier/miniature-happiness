41a9bbefebeb0f02b8dbb84d9b45bef3f3290e0a
emilypier <piersoee@miamioh.edu>
2022-05-18 21:22:26 -0400

    character count

--- algorithms/09-Algorithms/01-character-count/Unsolved/character-count.js
+++ algorithms/09-Algorithms/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function(str) {
+  // create a new object
+  let charCount = {};
+
+  // loop through each char in string and create a new
+  // key for each char in string
+
+  // value of char's key should be the number of times
+  // it appears in string
+
+  
+};
