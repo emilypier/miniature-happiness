bf21be0c127852130149b002721fd3f15f93c314
emilypier <piersoee@miamioh.edu>
2022-05-31 22:05:40 -0400

    unique values in array algorithm

--- algorithms/010-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ algorithms/010-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,20 @@
 // TODO: Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+  let numMap = {};
+
+  //loop through array
+  for (let i = 0; i < arr.length; i++ ) {
+    let currentNumber = arr[i];
+
+    if (numMap[currentNumber] === true) {
+      return false;
+    }
+
+    numMap[currentNumber] = true;
+  }
+
+  return true;
+  
+};
