4c9b971a02f767fc3fcb545189e3f5cf92efb885
emilypier <piersoee@miamioh.edu>
2022-05-31 22:45:56 -0400

    anagram algorithm

--- algorithms/010-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ algorithms/010-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,8 @@
 // TODO: Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+  // reverse string?
+  // if string is anagram, return true
+  // if not, return false
+};
