ba3448ca0d29ad500cf54c38cb7a65cbe6582214
emilypier <piersoee@miamioh.edu>
2022-06-02 17:20:25 -0400

    anagram algorithm attempt

--- algorithms/010-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ algorithms/010-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -2,7 +2,13 @@
 // Return true if the strings are anagrams of each other, otherwise return false
 
 var isAnagram = function(strA, strB) {
-  // reverse string?
-  // if string is anagram, return true
+  // if strings aren't the same amount of characters, return false.
+  if (strA.length != strB.length) {
+    return false;
+  };
+  
+
+  // if strA === strB, return true
+  if stringArray === newStringArray
   // if not, return false
 };
