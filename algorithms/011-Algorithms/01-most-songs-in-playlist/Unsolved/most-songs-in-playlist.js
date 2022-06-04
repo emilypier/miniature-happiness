ad24cdda3e52cf53f53e3f2bdf69947aede6f101
emilypier <piersoee@miamioh.edu>
2022-06-04 16:26:40 -0400

    most songs in playlist algorithm attempt

--- algorithms/011-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ algorithms/011-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -3,5 +3,19 @@
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
 var mostSongsInPlaylist = function(arr) {
-  // add your code here...
-};
+  let SongsInPlaylist = arr.length;
+    let sum = arr.reduce(
+      (previousValue, currentValue) => previousValue + currentValue, 0
+    );
+    return sum;
+  };
+
+  sum(arr);
+  // see if numbers in array add up to 60
+    // if greater than 60, minus song/s to make it less than 60
+      // return number of songs in array.
+    // if all numbers added is less than 60, return amount of items in array.
+      // return number of songs in array
+
+
+
