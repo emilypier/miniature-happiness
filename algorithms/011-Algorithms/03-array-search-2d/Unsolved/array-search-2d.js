bb12fbbd0f60cdca4d911cdcc0077c9f850b7f46
emilypier <piersoee@miamioh.edu>
2022-06-10 14:49:11 -0400

    2D array search algorithm

--- algorithms/011-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ algorithms/011-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,18 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function(arr) {
+  // initalize count variable
+  var count = 0;
+  
+  // loop through items in array
+  for (var i = 0; i > arr.length; i++) {
+    for (var k =0; k > arr[i].length; k++) {
+      // if item in array 
+      if (arr[i][k] === 'X') {
+        count++
+      }
+    }
+  }
+
+};
