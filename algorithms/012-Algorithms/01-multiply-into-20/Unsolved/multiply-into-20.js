ecefb979846f9d6fd26eef79fa2abbf0981c8ed2
emilypier <piersoee@miamioh.edu>
2022-06-10 16:56:22 -0400

    attempt at multiply-into-20 algorithm

--- algorithms/012-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ algorithms/012-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -2,13 +2,10 @@ const multiplyInto20 = function(arr) {
 
   // loop through numbers in array
   for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
+    for (var j = i + 1; j < arr.length; j++) {
       if (i * j) {
         return true;
-      } else {
-        return false;
       }
-    }
-  }
-
+    };
+  };
 };
